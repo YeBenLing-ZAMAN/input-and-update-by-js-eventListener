9809a6d726549cd798a1cd81d11fe928c4b528bf
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-08 21:29:21 +0600

    add input a commit and show it

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.getElementById('eventListener').addEventListener('click', function(){
     updateText.innerText= 'set by using driect addEventListener function '
 })
 
-//  driect using by=
+//  states update by using driect function 
 document.getElementById('eventListenerUpdate').addEventListener('click',function(){
     const text= document.getElementById('updateText');
     // console.log(text.value);
@@ -23,5 +23,21 @@ document.getElementById('eventListenerUpdate').addEventListener('click',function
 
 
 
-// states update by using driect function 
+    //1. add Event listener in driect method 
+document.getElementById('submited-commit').addEventListener('click',function(){
+    //2.  get user comment
+    const commitBox = document.getElementById('commit-add');
+    // console.log(commitBox.value);
+
+    //3. create comment paragraph
+    const newComment = document.createElement('p');
+    newComment.innerText = commitBox.value; 
+
+    //4. append the commit 
+    const commentContainer = document.getElementById('comment-area');
+    commentContainer.appendChild(newComment);
+
+    //5. clear the commont box 
+    commitBox.value= '';
+})
 
